efcb6928bbc2cd4e6976c35095fb379a6d67cf19
ShaheerAliDanish <shaheeralidanish@gmail.com>
2023-12-07 03:05:03 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var lastname= "danish";
+var midname="Ali";
+var firstname="Shaheer";
+var marj=firstname+midname+lastname;
+alert (marj);
+console.log(marj);
+var height=5;
+var age=28;
+if (height>5 || age==18){
+   prompt("Enter Your id number");
+}
+else{
+    alert("not allow")
+}
+var arr=["20" , "10" , "15"]
+
+alert (arr)
+arr[3]=25;
+alert (arr)
+
+
